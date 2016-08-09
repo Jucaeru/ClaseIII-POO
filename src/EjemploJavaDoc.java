7efd56ab0f40a6375404dc663c26259e8f621bed
Estudiante <Estudiante@10.203.2.171>
2016-08-09 14:26:26 -0500

    La csm

---
+++ src/EjemploJavaDoc.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LACSM
+ * @since 08/08/2016
+ * @version 1.1
+ */
+public class EjemploJavaDoc {
+    public static void main (String[] arg){
+        System.out.print("gg pe csm");
+    } 
+}
